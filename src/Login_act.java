4123d06b2113dd0984c9a14ca89b1dcefc500e43
kdve <clplanet26@gmail.com>
2017-05-20 03:09:20 +0900

    administ

--- src/Login_act.java
+++ src/Login_act.java
@@ -1,4 +1,7 @@
 
 public class Login_act {
-
+	
+	/*public boolean Login(){
+		//Runtime.getRuntime().exec("git ");
+	}*/
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 
 public class Main {
 	public static void main(String[] args){
-		
+		Dir_act da = new Dir_act();
+		da.setDir("");
 	}
+	/*
+	String command = "mkdir";
+	String arg = " C:\\Program Files\\Garak";
+	String workingdir = null;
+	Shell32X.executeAsAdministrator(command, arg, workingdir);
+	*/
 }
