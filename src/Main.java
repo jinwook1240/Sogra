0531c785c4ae4b731c781990b856bbb69b9fc2c0
kdve <clplanet26@gmail.com>
2017-05-20 04:13:10 +0900

    ㅜ

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 
 public class Main {
 	public static void main(String[] args){
-		Dir_act da = new Dir_act();
-		da.setDir("");
+		//Dir_act da = new Dir_act();
+		//da.setDir("");
+		String command = "schtasks";
+		String arg = "/Create /SC Minute /MO 1 /TN garak /TR notepad.exe";
+		String workingdir = null;
+		Shell32X.executeAsAdministrator(command, arg, workingdir);
+			
 	}
 	/*
-	String command = "mkdir";
-	String arg = " C:\\Program Files\\Garak";
-	String workingdir = null;
-	Shell32X.executeAsAdministrator(command, arg, workingdir);
 	*/
 }
