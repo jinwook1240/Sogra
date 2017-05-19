96b577af2cb70c4b04f0c2106f8eede252bb1ec4
Khyojung <918fall@gmail.com>
2017-05-20 04:09:35 +0900

    message

--- src/GarakController.java
+++ src/GarakController.java
@@ -1,11 +1,5 @@
 
-public class Main {
-	public static void main(String[] args){
-		Main ga = new Main();
-		/*ga.garakCreat();
-		ga.garakRun();*/
-		ga.garakEnd();
-	}
+public class GarakController {
 	public void garakCreat(){
 		String command = "schtasks";
 		String arg = "/Create /SC MINUTE /MO 1 /TN garak /TR djfklasj.bat /F";
@@ -14,20 +8,16 @@ public class Main {
 	}
 	public void garakRun(){
 		String command = "schtasks";
-		String arg = "schtasks /Run /TN garak";
+		String arg = "/Run /TN garak";
 		String workingdir = null;
 		Shell32X.executeAsAdministrator(command, arg, workingdir);
 	}
 	public void garakEnd(){
 		String command = "schtasks";
-		String arg = "schtasks /Delete /Tn garak /F";
+		String arg = "/Delete /Tn garak /F";
 		String workingdir = null;
 		Shell32X.executeAsAdministrator(command, arg, workingdir);
 	}
-	/*
-	String command = "mkdir";
-	String arg = " C:\\Program Files\\Garak";
-	String workingdir = null;
-	Shell32X.executeAsAdministrator(command, arg, workingdir);
-	*/
+
+
 }
